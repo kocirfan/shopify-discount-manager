5b9340550de36afeedf410aa78b15efbc9b659cc
kocirfan <irfnk83@gmail.com>
2026-01-06 16:37:51 +0300

    Fix Vercel serverless function configuration

--- api/index.js
+++ api/index.js
@@ -1,4 +1,14 @@
 import { createRequestHandler } from "@react-router/node";
-import * as build from "../build/server/index.js";
 
-export default createRequestHandler({ build });
+export default async function handler(req, res) {
+  try {
+    // Dynamically import the build
+    const build = await import("../build/server/index.js");
+    const requestHandler = createRequestHandler({ build });
+
+    return requestHandler(req, res);
+  } catch (error) {
+    console.error("Handler error:", error);
+    return res.status(500).json({ error: error.message });
+  }
+}
